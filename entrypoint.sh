fa1203f4ce1e5bd5c06c346c00117730c00e77d1
Eve <arnaud.bouts@gmail.com>
2017-07-17 22:49:01 +0200

    fix domain sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # if first start, init conf
-sed -i "s/\${DOMAIN}/${DOMAIN}/1" /etc/spectrum2/transports/hangouts.cfg
+sed -i "s/{{DOMAIN}}/${DOMAIN}/1" /etc/spectrum2/transports/hangouts.cfg
 
 exec "$@"
